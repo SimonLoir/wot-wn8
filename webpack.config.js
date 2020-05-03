d313a2928c90e8b432d64095761aab72698d4b00
SimonLoir <simon-loir@hotmail.com>
2020-05-03 13:41:19 +0200

    created base structure

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = [
+    {
+        target: 'node',
+        entry: {
+            site: './src/client/typescript/index.ts',
+        },
+        node: false,
+        module: {
+            rules: [
+                {
+                    test: /\.ts?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: 'style-loader', // creates style nodes from JS strings
+                        },
+                        {
+                            loader: 'css-loader', // translates CSS into CommonJS
+                        },
+                        {
+                            loader: 'sass-loader', // compiles Sass to CSS
+                        },
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js', '.scss'],
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'public'),
+        },
+        watch: true,
+    },
+    {
+        target: 'node',
+        entry: {
+            app: './src/server/index.ts',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, '.'),
+        },
+        watch: true,
+        externals: nodeExternals(),
+    },
+];
