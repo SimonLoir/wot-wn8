f68904d304c8bf192fa123b94fe89fbc647a7144
SimonLoir <simon-loir@hotmail.com>
2020-05-09 22:55:44 +0200

    replaced the ad

--- src/client/typescript/ads.ts
+++ src/client/typescript/ads.ts
@@ -4,6 +4,6 @@ export function createAd(parent: ExtJsObject) {
     const el = parent.child('div').addClass('ad');
     el.css('textAlign', 'center');
     el.html(
-        `<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=8&p=48&l=ez&f=ifr&linkID=333e26cce8d73e149e8989fa81c47733&t=sloir-21&tracking_id=sloir-21" width="728" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>`
+        `<iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=8&p=48&l=ur1&category=logiciels&banner=1H8V96R69K5Z2K93T482&f=ifr&linkID=c248a8fb07f27f4a5a4fb5868122e8b6&t=sloir-21&tracking_id=sloir-21" width="728" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>`
     );
 }
