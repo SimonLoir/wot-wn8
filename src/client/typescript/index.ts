8bcb1b590b91a5f51fa7c99af7760de94a226d32
SimonLoir <simon-loir@hotmail.com>
2020-05-21 12:42:11 +0200

    working version of player storage system

--- src/client/typescript/index.ts
+++ src/client/typescript/index.ts
@@ -5,6 +5,15 @@ import { $ } from './extjs';
 const btn = document.getElementById('search');
 const search_bar: HTMLInputElement = document.querySelector('input#search_bar');
 const main = document.querySelector('#main');
+const render = (data: player_search_result_item[]) => {
+    main.innerHTML = '';
+    data.forEach((e) => {
+        main.appendChild(document.createElement('div')).innerHTML = `
+        <a href="${e.user_id}">${e.user_name}</a>
+        `;
+    });
+    if (data.length == 1) main.querySelector('a').click();
+};
 
 search_bar.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
@@ -13,18 +22,28 @@ search_bar.addEventListener('keypress', function (e) {
 });
 
 btn.addEventListener('click', async (e) => {
-    main.innerHTML = 'Loading data from Wargaming API';
-    const wot_player_search: wot_player_search = await (
-        await fetch(`api/player/${search_bar.value}`)
+    main.innerHTML = 'Loading data from servers';
+    const search: player_search_result_item[] = await (
+        await fetch(`api/player/${search_bar.value}/search`)
     ).json();
-
-    if (!wot_player_search.data) return alert('An error occured');
-    wot_player_search.data.map((e) => {
-        main.appendChild(document.createElement('div')).innerHTML = `
-        <a href="${e.account_id}">${e.nickname}</a>
-        `;
+    render(search);
+    const index = main.appendChild(document.createElement('button'));
+    index.innerHTML = `Player not found ?`;
+    index.addEventListener('click', async () => {
+        index.style.display = 'none';
+        const wot_player_search: wot_player_search = await (
+            await fetch(`api/player/${search_bar.value}`)
+        ).json();
+        if (!wot_player_search.data || wot_player_search.data.length == 0)
+            return alert('An error occured while getting data from WG API');
+        const data = wot_player_search.data.map((e) => {
+            return {
+                user_id: e.account_id,
+                user_name: e.nickname,
+            };
+        });
+        render(data);
     });
-    if (wot_player_search.data.length == 1) main.querySelector('a').click();
 });
 
 menu();
---
+++ src/interfaces/player_search_result.d.ts
@@ -0,0 +1,4 @@
+declare interface player_search_result_item {
+    user_id: number;
+    user_name: string;
+}
